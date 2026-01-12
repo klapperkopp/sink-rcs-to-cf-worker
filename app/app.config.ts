244b1784ea6fe1786116106dea01e5c86399f77b
Toni K. <11021127+klapperkopp@users.noreply.github.com>
2026-01-12 15:11:01 +0100

    Update app configuration for RCS Link Shortener

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,13 +1,13 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/miantiao-me/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
+  title: 'RCS Link Shortener',
+  email: 'toni.kuschan@vonage.com',
+  github: 'https://github.com/klapperkopp/sink-rcs-to-cf-worker',
+  twitter: 'https://vonage.com',
+  telegram: 'https://vonage.com',
+  mastodon: 'https://vonage.com',
+  blog: 'https://vonage.com',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  image: 'https://i.ytimg.com/vi/fnwOkrPmZ1o/maxresdefault.jpg',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
